12c23d22cde20e99242afefc342a92104acec767
hexagonstem <bishara@hexagon-stem.com>
2020-07-01 10:40:28 +0000

    updated API key

--- weather.py
+++ weather.py
@@ -7,7 +7,8 @@ def get_weather_data(place_list):
 
 url = "https://api.climacell.co/v3/weather/realtime"
 payload = {
-  "apikey": "MlzJxYVk8vU6tjV0U4SNf9WKItn4fTzq","lat":35.7767, 
+  "apikey": "jUYwtd18Y4T4B7V0yhlWB1VXA33Owcfl
+","lat":35.7767, 
   "lon":140.3187,
   "fields": ["temp", "precipitation", "wind_gust"],
   "unit_system":"us", 
